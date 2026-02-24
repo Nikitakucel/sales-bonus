ad29b55c3cd47a0e261e6aa3cb43f1498762e91e
nikita grebyonkin <nikita05santa@gmail.com>
2026-02-24 19:04:36 +0300

    исправление ошибок

--- src/main.js
+++ src/main.js
@@ -101,7 +101,7 @@ function analyzeSalesData(data, options) {
     });
 
     // @TODO: Сортировка продавцов по прибыли
-    const sortedSellers = sellerStats.toSorted((a, b) => b.profit - a.profit);
+    const sortedSellers = sellerStats.sort((a, b) => b.profit - a.profit);
 
     // @TODO: Назначение премий на основе ранжирования
     sortedSellers.forEach((seller, index) => {
